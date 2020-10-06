5ea6e5fdb5328adf93ef5f664976fdae14691456
Gabriel P Samson <gabriel.samson@segment.com>
2020-10-06 11:42:59 -0700

    Update more imports

--- Pod/Classes/SEGCrittercismIntegration.m
+++ Pod/Classes/SEGCrittercismIntegration.m
@@ -1,7 +1,15 @@
 #import "SEGCrittercismIntegration.h"
 #import <Crittercism/Crittercism.h>
+#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
 #import <Analytics/SEGAnalyticsUtils.h>
+#else
+#import <Segment/SEGAnalyticsUtils.h>
+#endif
+#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
 #import <Analytics/SEGAnalytics.h>
+#else
+#import <Segment/SEGAnalytics.h>
+#endif
 
 
 @implementation SEGCrittercismIntegration
